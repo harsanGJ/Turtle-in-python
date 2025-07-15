3f0827b46110fce656b66f88e348ef33c6184429
harsanGJ <gunarenu0@gmail.com>
2025-07-15 21:22:18 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+polygon=turtle.Turtle()
+for i in range(6):
+    polygon.forward(70)
+    polygon.right(60)
+turtle.done()
